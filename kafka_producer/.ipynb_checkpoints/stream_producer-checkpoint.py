a82e1a48cd0341c2df50e8d48362ac10dc6111df
mrafalo <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>
2022-08-28 09:26:31 -0400

    kafka producer
    kafka producer

--- kafka_producer/.ipynb_checkpoints/stream_producer-checkpoint.py
+++ kafka_producer/.ipynb_checkpoints/stream_producer-checkpoint.py
@@ -10,30 +10,26 @@ with open(r'config.yaml') as file:
 
     KAFKA_SERVER = cfg['global']['bootstrap_server']
     DATA_FILE = cfg['global']['data_file']
-    KAFKA_TOPIC =cfg['global']['topic']
-    LINGER_MS =cfg['producer']['linger_ms']
-    DELAY_SEC =cfg['global']['delay_sec']
-    BATCH_SIZE =cfg['producer']['batch_size']
-    RECORD_CNT =cfg['global']['record_cnt']
-
-def success(metadata):
-    print(metadata.offset)
+    KAFKA_TOPIC = cfg['global']['topic']
+    LINGER_MS = cfg['producer']['linger_ms']
+    DELAY_SEC = cfg['global']['delay_sec']
+    BATCH_SIZE = cfg['producer']['batch_size']
+    RECORD_CNT = cfg['global']['record_cnt']
+    RUN_FLAG = cfg['global']['running']
 
 def error(exception):
     print(exception)
 
-def kafka_python_producer_async(_producer, _msg, _topic, _delay_sec):
-    #_producer.send(topic = _topic, value = _msg).add_callback(success).add_errback(error)
+def kafka_python_producer_async(_producer, _msg, _topic):
     _producer.send(topic = _topic, value = _msg).add_errback(error)
-    time.sleep(_delay_sec)
-
     _producer.flush()
     
-
-
 cnt = 0
+cnt_window = 0
 producer = KafkaProducer(bootstrap_servers = KAFKA_SERVER, linger_ms = LINGER_MS)
 exp_time = datetime.datetime.now().timestamp()
+
+WINDOW_INTERVAL = 20
 
 while True:
     data = csv.reader(open(DATA_FILE), delimiter=",")
@@ -48,8 +44,9 @@ while True:
         time_diff = datetime.datetime.now().timestamp() - exp_time
         
         
-        if time_diff>20:
-            print(datetime.datetime.now())
+        if time_diff>WINDOW_INTERVAL:
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "total events sent:", cnt, "last", WINDOW_INTERVAL, "seconds:", cnt_window, "running:", RUN_FLAG)
+            cnt_window = 0
             exp_time = datetime.datetime.now().timestamp()
             
             with open(r'config.yaml') as file:
@@ -57,14 +54,16 @@ while True:
 
                 KAFKA_SERVER = cfg['global']['bootstrap_server']
                 DATA_FILE = cfg['global']['data_file']
-                KAFKA_TOPIC =cfg['global']['topic']
-                LINGER_MS =cfg['producer']['linger_ms']
-                DELAY_SEC =cfg['global']['delay_sec']
-                BATCH_SIZE =cfg['producer']['batch_size']
+                KAFKA_TOPIC = cfg['global']['topic']
+                LINGER_MS = cfg['producer']['linger_ms']
+                DELAY_SEC = cfg['global']['delay_sec']
+                BATCH_SIZE = cfg['producer']['batch_size']
+                RECORD_CNT = cfg['global']['record_cnt']
+                RUN_FLAG = cfg['global']['running']
                 
             producer = KafkaProducer(bootstrap_servers = KAFKA_SERVER, linger_ms = LINGER_MS)
             
-        cnt = cnt + 1
+        
         msg = r
         msg.append(KAFKA_TOPIC)
         msg.append(DELAY_SEC)
@@ -72,15 +71,22 @@ while True:
         msg.append(LINGER_MS)    
         msg.append(datetime.datetime.now().timestamp())    
         msg_flat = ';'.join(str(m) for m in msg)
-        kafka_python_producer_async(producer, msg_flat.encode(), KAFKA_TOPIC, DELAY_SEC)
-
+        
+        if RUN_FLAG == 1:
+            kafka_python_producer_async(producer, msg_flat.encode(), KAFKA_TOPIC)
+            cnt = cnt + 1
+            cnt_window = cnt_window + 1
+        
+        time.sleep(DELAY_SEC)
+        
         #print(msg_flat)
 
         if (cnt > RECORD_CNT) and (RECORD_CNT > 0):
-            print("Maximum number of record reached: ", RECORD_CNT)
+            print("Maximum number of events reached:", RECORD_CNT)
             break
         
     if (cnt > RECORD_CNT) and (RECORD_CNT > 0):
+        print("Maximum number of events reached:", RECORD_CNT)
         break
     
     
